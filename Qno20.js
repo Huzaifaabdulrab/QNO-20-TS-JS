05684acd5e49797dea80d0d0d5402f73bdd78a92
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:41:28 +0500

    Add files via upload

---
+++ Qno20.js
@@ -0,0 +1,8 @@
+"use strict";
+// 20 .Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let countries = ["Japan", "Dubai", "Sudia", "Iraq"];
+//simple methode;
+console.log("I like to visit :", countries);
+//map methode;
+countries.map((count) => console.log(`I like to visit ${count} country`));
